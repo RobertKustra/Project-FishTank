7285b0446c22573420c01427017b9943ee9c306d
RobertK <robert.kustra@outlook.com>
2022-05-18 18:21:14 +0200

    Add-Cors

--- Project-FishTank/FishTank.API/Program.cs
+++ Project-FishTank/FishTank.API/Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "RestrictArea51",
+    builder =>
+    {
+        builder.AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
