0194c2c7c58fbdedb0dced6fc7c5f21c92295801
RobertK <robert.kustra@outlook.com>
2022-05-18 22:48:14 +0200

    Lesson9

---
+++ Project-FishTank/FishTank.Domain/Entities/LivingEnvironment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTank.Domain.Entities
+{
+    public class LivingEnvironment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WaterType { get; set; }
+        public ICollection<Fish> Fish { get; set; }
+    }
+}
---
+++ Project-FishTank/FishTank.Persistance/FishDbContext.cs
@@ -0,0 +1,52 @@
+﻿using FishTank.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTank.Persistance
+{
+    public  class FishDbContext : DbContext
+    {
+        public FishDbContext(DbContextOptions<FishDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach(var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = string.Empty;
+                        entry.Entity.Created = DateTime.Now;
+                        entry.Entity.StatusId = 1;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.Modified = DateTime.Now;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.Modified = DateTime.Now;
+                        entry.Entity.Inactivated = DateTime.Now;
+                        entry.Entity.InactivatedBy = String.Empty;
+                        entry.Entity.StatusId = 0;
+                        entry.State = EntityState.Modified;
+                        break;
+
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
