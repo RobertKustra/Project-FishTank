3e96420c7d49b9af6f9e1c3dfa7cd2efce24cc72
RobertK <robert.kustra@outlook.com>
2022-06-12 17:00:49 +0200

    Add IDateTime Interface

---
+++ Project-FishTank/FishTank.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FishTank.Application.Common.Interfaces;
+using FishTank.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTank.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDateTime, DateTimeService>();
+            return services;
+        }
+    }
+}
